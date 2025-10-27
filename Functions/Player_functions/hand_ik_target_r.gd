d8e1d301c9b49dc13f62df2004ceb8759a1ec2b5
XspyO5 <tommaso.vanni.tv@gmail.com>
2025-10-27 23:36:22 +0100

    a0.7, further debug needed for hands movement, but almost done

---
+++ Functions/Player_functions/hand_ik_target_r.gd
@@ -0,0 +1,4 @@
+extends HandIkTarget
+
+func _physics_process(delta: float) -> void:
+	Update(delta)
--- Functions/Player_functions/ik_class.gd
+++ Functions/Player_functions/ik_class.gd
@@ -2,7 +2,7 @@ class_name HandIkTarget
 
 extends Node2D
 
-@export var side_RorL : String 
+@export var is_right : bool
 @export var hand_node : Node2D
 @export var skeleton_node : Node2D
 @onready var mouse_positon = get_viewport().get_mouse_position()
@@ -11,9 +11,14 @@ var self_angle = 0.0
 var mouse_angle = 0.0
 var self_distance = 0.0
 var mouse_distance = 0.0
+var lower_limit #minimum angle
+var upper_limit #maximum angle
+var max_distance = 350 #maxiumum distance
 
-func go_to(object, target):
-	object.set_global_position(target.get_global_position())
+
+
+func move(target, speed):
+	position = position.move_toward(target, speed)
 
 func calc_mouse_distance():
 	return sqrt( (skeleton_node.global_position.x - mouse_positon.x)**2 + (skeleton_node.global_position.y - mouse_positon.y)**2 )
@@ -22,15 +27,17 @@ func calc_self_distance():
 	return sqrt( (skeleton_node.global_position.x - hand_node.global_position.x)**2 + (skeleton_node.global_position.y - hand_node.global_position.y)**2 )
 
 func calc_self_angle():
-	var result = atan2(skeleton_node.global_position.y - hand_node.global_position.y , skeleton_node.global_position.x - hand_node.global_position.x)
-	if(side_RorL == "R"):
-		result = 180 - result
+	var result = atan2((hand_node.global_position.y - skeleton_node.global_position.y) , (hand_node.global_position.x - skeleton_node.global_position.x)) + PI
 	return result
 	
 func calc_mouse_angle():
-	var result = atan2(skeleton_node.global_position.y - mouse_positon.y , skeleton_node.global_position.x - mouse_positon.x)
-	if(side_RorL == "R"):
-		result = PI - result #fix R side for angle grater then 180 sice it use a 0 to 360 scale instead of a -180 to 180 scale
+	var result = atan2(mouse_positon.y - skeleton_node.global_position.y, mouse_positon.x - skeleton_node.global_position.x )
+	#if(is_right):
+	#	if(result > 0):
+	#		result = PI - result
+	#	else:
+	#		result = -PI - result
+	
 	return result
 	
 func Update(_delta):
@@ -38,6 +45,11 @@ func Update(_delta):
 	self_angle = calc_self_angle()
 	mouse_angle = calc_mouse_angle()
 	self_distance = calc_self_distance()
-	mouse_distance = calc_self_angle()
+	mouse_distance = calc_mouse_distance()
 	
-	print(side_RorL," - ",rad_to_deg(mouse_angle))
+	self_distance = mouse_distance
+	
+	self_angle = lerp_angle(self_angle, mouse_angle, 1)
+	
+	global_position = Vector2(self_distance*cos(self_angle) + skeleton_node.global_position.x, self_distance*sin(self_angle) + skeleton_node.global_position.y)
+	print(rad_to_deg(self_angle)," - ",rad_to_deg(mouse_angle)," - ",lerp_angle(self_angle, mouse_angle, 0.51))
