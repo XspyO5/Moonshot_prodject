d042ed4146332398262fbb701ee3914a7c81d9bb
XspyO5 <tommaso.vanni.tv@gmail.com>
2025-10-29 22:51:25 +0100

    a0.8, hand rotation done, limit yet to setup

--- Functions/Player_functions/ik_class.gd
+++ Functions/Player_functions/ik_class.gd
@@ -6,15 +6,27 @@ extends Node2D
 @export var hand_node : Node2D
 @export var skeleton_node : Node2D
 @onready var mouse_positon = get_viewport().get_mouse_position()
+@onready var core_node = $"../.."
 
 var self_angle = 0.0
 var mouse_angle = 0.0
+var value_ang_difference: float
 var self_distance = 0.0
 var mouse_distance = 0.0
-var lower_limit #minimum angle
-var upper_limit #maximum angle
+var lower_limit: float #minimum angle
+var upper_limit: float #maximum angle
 var max_distance = 350 #maxiumum distance
+var min_distance = 100 #maxiumum distance - yet to define
+var anglular_speed: float
 
+func calculate_limit():
+	upper_limit =  3.0/4.0 * PI + core_node.global_rotation
+	lower_limit =  -2.0/3.0 * PI + core_node.global_rotation
+	print("h -",lower_limit)
+	
+	if(is_right == true):
+		lower_limit = PI - lower_limit
+		upper_limit = PI - upper_limit
 
 
 func move(target, speed):
@@ -27,7 +39,7 @@ func calc_self_distance():
 	return sqrt( (skeleton_node.global_position.x - hand_node.global_position.x)**2 + (skeleton_node.global_position.y - hand_node.global_position.y)**2 )
 
 func calc_self_angle():
-	var result = atan2((hand_node.global_position.y - skeleton_node.global_position.y) , (hand_node.global_position.x - skeleton_node.global_position.x)) + PI
+	var result = atan2((hand_node.global_position.y - skeleton_node.global_position.y) , (hand_node.global_position.x - skeleton_node.global_position.x))
 	return result
 	
 func calc_mouse_angle():
@@ -46,10 +58,16 @@ func Update(_delta):
 	mouse_angle = calc_mouse_angle()
 	self_distance = calc_self_distance()
 	mouse_distance = calc_mouse_distance()
+	calculate_limit()
+	
+	value_ang_difference = angle_difference(self_angle, mouse_angle)
 	
 	self_distance = mouse_distance
 	
-	self_angle = lerp_angle(self_angle, mouse_angle, 1)
+	if((value_ang_difference < 0 && self_angle < upper_limit) || (value_ang_difference > 0 && self_angle > lower_limit)):
+		print("it works")
+		self_angle = self_angle + value_ang_difference*self_distance/3500
+	
 	
 	global_position = Vector2(self_distance*cos(self_angle) + skeleton_node.global_position.x, self_distance*sin(self_angle) + skeleton_node.global_position.y)
-	print(rad_to_deg(self_angle)," - ",rad_to_deg(mouse_angle)," - ",lerp_angle(self_angle, mouse_angle, 0.51))
+	print(hand_node," - ",self_angle," - ",upper_limit," - ",self_angle > lower_limit, "-", value_ang_difference > 0)
